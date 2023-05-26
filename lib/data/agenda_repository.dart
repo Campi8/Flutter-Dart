175699fd7e119a84c8e5e92a9847c4f2f85ed1ba
Campi8 <gustavocampi02@gmail.com>
2023-05-26 20:58:00 -0300

    branch com o código da aula do dia Fri May 26 20:57:58     2023

---
+++ lib/data/agenda_repository.dart
@@ -0,0 +1,47 @@
+import 'package:demo/domain/agenda.dart';
+
+List<Agenda> loadAgenda(){
+  return <Agenda>[
+
+    Agenda(
+      nome: "Joao da Silva",
+      foto: "https://www.gov.br/pt-br/noticias/assistencia-social/2022/11/publicada-regras-para-preenchimento-da-carteira-de-identidade-nacional/1200b9af-7d34-417e-b181-cfa8c4ec8f50.png",
+      email: "joao@gmail.com",
+      data: "29-05-2001",
+      idade: 22,
+      telefone: "(16)998-3445",
+    ),
+
+
+    Agenda(
+      nome: "kamily mandela",
+      foto: "https://www.gov.br/pt-br/noticias/assistencia-social/2022/11/publicada-regras-para-preenchimento-da-carteira-de-identidade-nacional/1200b9af-7d34-417e-b181-cfa8c4ec8f50.png",
+      email: "",
+      data: "",
+      idade: 23,
+      telefone: "",
+    ),
+
+
+    Agenda(
+      nome: "Gustavo Narciuso",
+      foto: "https://www.gov.br/pt-br/noticias/assistencia-social/2022/11/publicada-regras-para-preenchimento-da-carteira-de-identidade-nacional/1200b9af-7d34-417e-b181-cfa8c4ec8f50.png",
+      email: "",
+      data: "",
+      idade: 23,
+      telefone: "",
+    ),
+
+
+    Agenda(
+      nome: "Eliseu Btencuourt",
+      foto: "https://www.gov.br/pt-br/noticias/assistencia-social/2022/11/publicada-regras-para-preenchimento-da-carteira-de-identidade-nacional/1200b9af-7d34-417e-b181-cfa8c4ec8f50.png",
+      email: "",
+      data: "",
+      idade: 23,
+      telefone: "",
+    ),
+
+
+  ];
+}
